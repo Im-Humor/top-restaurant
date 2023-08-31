986c5652a0b1b68455ac65b442a37c4ced79ddad
Im-Humor <orangite@gmail.com>
2023-08-31 10:25:02 -0700

    add sourcemap for debugging, enable htmlwebpackplugin with template, add css import into js source file for css loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,27 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'eval',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
