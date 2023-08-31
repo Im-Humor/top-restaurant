8a5da79c4e6a75f11f08c9bc8d9d33404cd535d2
Im-Humor <orangite@gmail.com>
2023-08-31 10:51:18 -0700

    add createAppend function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './style.css';
 
+export default function createAppend(objectType, appendedParent, newClass = 0) {
+    const newElement = document.createElement(objectType);
+    if (typeof newClass == 'string') {
+        newElement.classList.add(newClass)
+    }
+    appendedParent.appendChild(newElement);
+    return newElement;
+}
+
+const pageContent = document.querySelector("#content");
+
+const testObject = createAppend("div", pageContent, "header");
+testObject.innerText = "hi";
+
+
 console.log("Hello, butthead!");
